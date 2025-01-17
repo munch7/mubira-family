1e34cb5a0bee8f435bda903f7c23231caddff65f
Francis <51071976+munch7@users.noreply.github.com>
2025-01-17 21:49:10 +0300

    hash

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,16 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideClientHydration } from '@angular/platform-browser';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), provideClientHydration(),
+    provideAnimationsAsync(), 
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 };
