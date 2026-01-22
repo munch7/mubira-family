adf50ff0356ef80c99d37e52ad48f12afb7f2b8a
Francis <51071976+munch7@users.noreply.github.com>
2026-01-22 13:15:19 +0300

    Revert "updates, home and about"
    This reverts commit d03219a574f8a42fef165b138d4ed63ff52696a7.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HttpClientModule, RouterModule],
+  imports: [HttpClientModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
